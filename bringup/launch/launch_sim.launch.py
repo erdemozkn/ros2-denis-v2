572a76fded93410c5b0b7048f778342b5386cca5
erdemozkn <erdemzkn7@gmail.com>
2026-05-25 20:51:52 +0300

    first init

---
+++ bringup/launch/launch_sim.launch.py
@@ -0,0 +1,100 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+
+
+def generate_launch_description():
+    pkg_denis_bringup = get_package_share_directory('bringup')
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(pkg_denis_bringup, 'launch', 'rsp.launch.py')
+        ]),
+        launch_arguments={
+            'use_sim_time': 'true',
+            'use_ros2_control': 'true'
+        }.items()
+    )
+
+    default_world = os.path.join(pkg_denis_bringup, 'world', 'gazebo_urban_station.sdf')
+
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
+            )
+        ]),
+        launch_arguments={
+            'gz_args': LaunchConfiguration('gz_args', default=['-r -v 4 ', world]),
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', '/robot_description',
+            '-name', 'denis',
+            '-x', '-1.0',
+            '-y', '1.0',
+            '-z', '20.0'
+        ],
+        output='screen'
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager-timeout", "35.0"],
+        output='screen'
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", "--controller-manager-timeout", "35.0"],
+        output='screen'
+    )
+
+    bridge_params = os.path.join(pkg_denis_bringup, 'config', 'bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=['--ros-args', '-p', f'config_file:={bridge_params}'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        rsp,
+        world_arg,
+        gazebo,
+        spawn_entity,
+        ros_gz_bridge,
+        TimerAction(
+            period=6.0,
+            actions=[
+                joint_broad_spawner,
+                diff_drive_spawner
+            ]
+        ),
+    ])
